b03aebef3a01b52538c0a466819efa3103b52a6c
s1medK1y <samedkaryagdi67@gmail.com>
2025-11-27 16:36:42 +0300

    Listeleme, arama ve güncelleme fonksiyonları tamamlandı

--- main.c
+++ main.c
@@ -8,6 +8,58 @@ struct Urun
     float fiyat;
     int stok;
 };
+void listele(struct Urun *urunler, int n)
+{
+
+    printf("\n\n----URUN LİSTESİ----\n\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("\nUrun Adi: %s \t Urun fiyati: %0.01f \t Stok Adedi: %d\n", urunler[i].urunAdi, urunler[i].fiyat, urunler[i].stok);
+    }
+}
+void ara(struct Urun *urunler, int n)
+{
+    char araİsim[30];
+    printf("Aranacak ürünün ismini girin:");
+    scanf("%s", araİsim);
+    int bulundu = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (strcmp(araİsim, urunler[i].urunAdi) == 0)
+        {
+            bulundu = 1;
+            printf("Aradiginiz Urun:\nUrun adi: %s\tUrun Fiyati:%0.01f\tStok Adedi: %d\n", urunler[i].urunAdi, urunler[i].fiyat, urunler[i].stok);
+            break;
+        }
+    }
+    if (!bulundu)
+    {
+        printf("Aradiginiz urun bulunamadi!");
+    }
+}
+void guncelle(struct Urun *urunler, int n)
+{
+    char updateName[30];
+    printf("Guncellemek istedigin urun ismini gir:");
+    scanf("%s", updateName);
+    int bulundu = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (strcmp(updateName, urunler[i].urunAdi) == 0)
+        {
+            printf("%s 'nin;\nYeni Fiyati:", urunler[i].urunAdi);
+            scanf("%f", &urunler[i].fiyat);
+            printf("%s 'nin;\nYeni Stok Adedi:", urunler[i].urunAdi);
+            scanf("%d", &urunler[i].stok);
+            bulundu = 1;
+            break;
+        }
+    }
+    if (!bulundu)
+    {
+        printf("Aradiginiz urun bulunamadi!");
+    }
+}
 
 int main()
 {
@@ -25,19 +77,39 @@ int main()
     }
     else
     {
-        for (i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
         {
-            printf("Urun adini giriniz: ");
+            printf("Urun adini giriniz:");
             scanf("%s", urunler[i].urunAdi);
             printf("Urun fiyatini giriniz: ");
             scanf("%f", &urunler[i].fiyat);
-            printf("Urunun stok fiyatini giriniz: ");
+            printf("Urunun stok adedini giriniz: ");
             scanf("%d", &urunler[i].stok);
         }
-        printf("\n\n----URUN LİSTESİ----\n\n");
-        for (i = 0; i < n; i++)
+        int secim;
+        while (1)
         {
-            printf("\nUrun Adi: %s \t Urun fiyati: %0.01f \t Stok Adedi: %d\n", urunler[i].urunAdi, urunler[i].fiyat, urunler[i].stok);
+            printf("\n1- Listele\n2- Ara\n3- Güncelle\n4- Cikis\nSecim: ");
+            scanf("%d", &secim);
+
+            if (secim == 1)
+            {
+                listele(urunler, n);
+            }
+            else if (secim == 2)
+            {
+                ara(urunler, n);
+            }
+            else if (secim == 3)
+            {
+                guncelle(urunler, n);
+            }
+            else if (secim == 4)
+                break;
+            else
+            {
+                printf("Lutfen deger araligindan bir sayi giriniz.");
+            }
         }
     }
     free(urunler);
